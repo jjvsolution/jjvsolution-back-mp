96d37d127aaf069ee50a3b0657ab23e4b7a3f9bb
Juan Carlos Venegas Zurita <juan.venegas1992@gmail.com>
2025-12-22 16:38:37 -0300

    dev

--- apps/jjvsolution-back-mp/src/app.module.ts
+++ apps/jjvsolution-back-mp/src/app.module.ts
@@ -22,7 +22,7 @@ import { PaymentPortalModule } from 'apps/payment-portal/src/payment-portal.modu
       driver: ApolloDriver,
       useFactory: (config: ConfigService<ConfigurationsInterface>) => {
         const plugins =
-          ['LOCAL', 'DEV'].includes(config.get<string>('ENVIRONMENT')?.toUpperCase())
+          ['LOCAL', 'DEV'].includes(`${config.get<string>('ENVIRONMENT')?.toUpperCase()}`)
             ? [ApolloServerPluginLandingPageLocalDefault()]
             : [];
         return {
